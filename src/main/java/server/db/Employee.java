e2c73680c788e83585c740152fee6187a89801cf
amidupeuple <danyapivovarov@gmail.com>
2013-09-20 16:41:05 +0400

    Second commit

--- src/main/java/server/db/Employee.java
+++ src/main/java/server/db/Employee.java
@@ -8,39 +8,49 @@ package server.db;
  * To change this template use File | Settings | File Templates.
  */
 public class Employee {
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
     private int id;
     private String firstName;
     private String lastName;
     private int salary;
 
     public Employee() {}
+
     public Employee(String fname, String lname, int salary) {
         this.firstName = fname;
         this.lastName = lname;
         this.salary = salary;
     }
-    public int getId() {
-        return id;
-    }
-    public void setId( int id ) {
-        this.id = id;
-    }
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName( String first_name ) {
-        this.firstName = first_name;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName( String last_name ) {
-        this.lastName = last_name;
-    }
-    public int getSalary() {
-        return salary;
-    }
-    public void setSalary( int salary ) {
-        this.salary = salary;
-    }
+
 }
